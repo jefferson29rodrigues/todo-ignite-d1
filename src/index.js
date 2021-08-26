65767919a5e9dc60d10d34d01bae0cf7f5a33dd0
Jefferson Rodrigues <jeffersonrodrigues5030@gmail.com>
2021-08-26 18:18:17 -0300

    retornando o ultimo item do array

--- src/index.js
+++ src/index.js
@@ -25,7 +25,7 @@ app.post('/users', (request, response) => {
     todos: []
   });
 
-  return response.status(201).json(users);
+  return response.status(201).json(users[users.length - 1]);
 });
 
 app.get('/todos', checksExistsUserAccount, (request, response) => {
